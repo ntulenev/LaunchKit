3a421955900bbe544eb970deeda0d2808761dfa5
Nikita Tulenev <n1tulenev@gmail.com>
2026-03-30 09:23:22 +0200

    Add XML docs to IConsoleRenderer

--- src/Abstractions/IConsoleRenderer.cs
+++ src/Abstractions/IConsoleRenderer.cs
@@ -2,7 +2,15 @@ using Models;
 
 namespace Abstractions;
 
+/// <summary>
+/// Renders the launcher user interface.
+/// </summary>
 public interface IConsoleRenderer
 {
+    /// <summary>
+    /// Starts the console UI for the provided launcher options.
+    /// </summary>
+    /// <param name="options">Launcher options to present to the user.</param>
+    /// <param name="cancellationToken">Token used to stop the UI loop.</param>
     Task RunAsync(LauncherOptions options, CancellationToken cancellationToken);
 }
