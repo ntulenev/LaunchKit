dc21d2cc30bacebbc3794bd758e154e3c5bbdedc
Nikita Tulenev <n1tulenev@gmail.com>
2026-03-30 09:23:22 +0200

    Add XML docs to IApplication

--- src/Abstractions/IApplication.cs
+++ src/Abstractions/IApplication.cs
@@ -1,6 +1,13 @@
 namespace Abstractions;
 
+/// <summary>
+/// Represents the application entry workflow.
+/// </summary>
 public interface IApplication
 {
+    /// <summary>
+    /// Runs the application workflow.
+    /// </summary>
+    /// <param name="cancellationToken">Token used to stop the workflow.</param>
     Task RunAsync(CancellationToken cancellationToken);
 }
